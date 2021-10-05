5c4270efd586e4a31d21dca343e09ba7df2b9f4c
skiwasaki <cask1200226@gn.iwasaki.ac.jp>
2021-10-05 11:55:52 +0900

    n1201022さんが変更

--- app/javascript/packs/home/index.js
+++ app/javascript/packs/home/index.js
@@ -5,6 +5,7 @@
 $(document).ready(function (){
     $(".title-image").fadeIn(1500,function (){
         $(".title-text").animate({opacity: 1},1000)
+        $(".buttons").animate({opacity: 1},1000)
     })
 });
 
