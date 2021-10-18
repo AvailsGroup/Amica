eb3a4ca1a02911913f4a35579b79611f309ba5ba
yurisi0212 <dev.yurisi@gmail.com>
2021-10-18 10:24:48 +0900

    権限の追加

--- Gemfile
+++ Gemfile
@@ -61,7 +61,6 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-
 gem 'devise'
 
 gem 'devise-i18n'
@@ -70,3 +69,4 @@ gem 'devise-i18n-views'
 
 gem 'omniauth'
 
+gem 'cancancan'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_032725) do
+ActiveRecord::Schema.define(version: 2021_10_18_012301) do
 
   create_table "inquiries", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_10_12_032725) do
     t.string "name"
     t.string "nickname"
     t.string "userid"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
