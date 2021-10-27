dce3d323bfac601dccd4634a2cf0d2c874f88840
n1200274 <cadu1200274@gn.iwasaki.ac.jp>
2021-10-27 11:43:58 +0900

    仮実装

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,8 +1,18 @@
 class CommunitiesController < ApplicationController
   def index
-
+    @community = Community.all
   end
   def new
     @community = Community.new
+  end
+  def create
+    @community = Community.new(community_params)
+    if @community.save
+    redirect_to(communities_path)
+    end
+  end
+  private
+  def community_params
+    params.require(:community).permit(:name,:content)
   end
 end
