e4fc5d058749e83902689f6b2661c3549c96f6e8
yurisi0212 <dev.yurisi@gmail.com>
2021-11-02 23:39:18 +0900

    日本語化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,9 +1,11 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @user = User.find_by(userid: params[:profile_id])
     @relationship = Relationship.new(follower_id: current_user.id, followed_id: @user.id)
     if @relationship.save!
-      redirect_to profile_path(params[:profile_id]), notice: "フォローしました！"
+      redirect_to profile_path(params[:profile_id]), notice: "友達申請をしました！"
     else
       redirect_to profile_path(params[:profile_id]), notice: "エラーが発生しました！"
     end
@@ -14,7 +16,7 @@ class RelationshipsController < ApplicationController
   def destroy
     other_user = User.find_by(userid: params[:profile_id])
     current_user.unfollow(other_user)
-    redirect_to profile_path(params[:profile_id]), notice: "フォローを外しました！"
+    redirect_to profile_path(params[:profile_id]), notice: "友達申請を取消しました！"
   end
 
 end
