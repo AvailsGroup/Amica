1fd0a9c56ca214a7ff092c789bfad52b9bebc3a3
n1200274 <cadu1200274@gn.iwasaki.ac.jp>
2021-11-10 11:41:57 +0900

    仮実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_020847) do
+ActiveRecord::Schema.define(version: 2021_11_10_023152) do
 
   create_table "communities", force: :cascade do |t|
     t.string "name"
