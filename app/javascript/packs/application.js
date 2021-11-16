0e55996fa9cf1d555f8bee2a5c2c7cdb79218582
n1200274 <cadu1200274@gn.iwasaki.ac.jp>
2021-11-16 09:08:21 +0900

    仮実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,3 +21,11 @@ require("@rails/activestorage").start()
 require("channels")
 // 追記
 require('jquery')
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+import "bootstrap";
+import "../stylesheets/application";
