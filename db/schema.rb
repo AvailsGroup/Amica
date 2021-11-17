105044e4327e6d491a482e794728be970d1bd548
n1200274 <cadu1200274@gn.iwasaki.ac.jp>
2021-11-17 11:45:52 +0900

    コミュニティタグの実装を開始

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_011321) do
+ActiveRecord::Schema.define(version: 2021_11_17_022256) do
 
   create_table "communities", force: :cascade do |t|
     t.string "name"
