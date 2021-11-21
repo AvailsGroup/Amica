5c514c7c2b176d44010f9fe38dfff7e24f010f72
skiwasaki <cask1200226@gn.iwasaki.ac.jp>
2021-11-22 08:45:40 +0900

    navのbarの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_124210) do
+ActiveRecord::Schema.define(version: 2021_11_17_015654) do
 
   create_table "inquiries", force: :cascade do |t|
     t.string "name"
