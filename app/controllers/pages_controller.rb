ec578e023855d56702dbaf15cbb5bb8dd53560aa
n1200245 <cars1200245@gn.iwasaki.ac.jp>
2021-11-23 11:55:14 +0900

    一時変更

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,10 @@ class PagesController < ApplicationController
   before_action :sign_in_required, only: [:show]
 
   def index
+    @mates = current_user.matchers
   end
 
   def show
-  end
 
+  end
 end
