e9456463abca1516a79472d6e8371b76dcbb1d46
n1200294 <cake1200294@gn.iwasaki.ac.jp>
2021-11-24 16:16:26 +0900

    いいね非同期完了！

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,11 +3,14 @@ class LikesController < ApplicationController
   def create
     Like.create(user_id: current_user.id, post_id: params[:timeline_id])
     @post = Post.find_by(id: params[:timeline_id])
+    @user = User.all
     #redirect_to timelines_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, post_id: params[:timeline_id]).destroy
-    redirect_to timelines_path
+    @post = Post.find_by(id: params[:timeline_id])
+    @user = User.all
+    #redirect_to timelines_path
   end
 end
