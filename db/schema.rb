30f4f8601d4c088dd257f61cb97721f3cf96a946
yurisi0212 <dev.yurisi@gmail.com>
2021-11-25 04:09:11 +0900

    警告機能の完成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_115735) do
+ActiveRecord::Schema.define(version: 2021_11_24_185111) do
 
   create_table "inquiries", force: :cascade do |t|
     t.string "name"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2021_11_24_115735) do
     t.boolean "admin", default: false
     t.string "image"
     t.boolean "ban", default: false
+    t.integer "warning", default: 0
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
