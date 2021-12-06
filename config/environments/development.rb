9115d551e6f0f5ba759002c6b851fd0322b7187c
n1200274 <cadu1200274@gn.iwasaki.ac.jp>
2021-12-06 11:24:33 +0900

    庭には二羽にわかがいるＷＷＷ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,9 +77,9 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' } # 追加
 
   #以下のコードは会員登録のテスト時に使ってください
-  #config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :letter_opener_web
 
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
