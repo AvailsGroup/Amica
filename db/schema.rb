b18453e2116c973b936c1947c375bd2126a54676
yurisi0212 <Dev.yurisi@gmail.com>
2021-12-07 17:02:22 +0900

    動かなかった原因を修正

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,12 @@ ActiveRecord::Schema.define(version: 2021_12_07_024841) do
     t.index ["user_id"], name: "index_community_members_on_user_id"
   end
 
+  create_table "community_tags", force: :cascade do |t|
+    t.integer "community_id"
+    t.string "tag"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
