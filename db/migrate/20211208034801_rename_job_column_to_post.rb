122f304eab67575ee7887513c94070379851c02b
yurisi0212 <Dev.yurisi@gmail.com>
2021-12-10 17:42:08 +0900

    マイグレーションファイルを修正

--- db/migrate/20211208034801_rename_job_column_to_post.rb
+++
@@ -1,5 +0,0 @@
-class RenameJobColumnToPost < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :posts, :job, :image
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_155307) do
+ActiveRecord::Schema.define(version: 2021_12_10_084055) do
 
   create_table "achievements", force: :cascade do |t|
     t.integer "user_id", null: false
