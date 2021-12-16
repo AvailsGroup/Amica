6efaa587e21bfacd04c97c13abfa0da4f79146ed
skiwasaki <cask1200226@gn.iwasaki.ac.jp>
2021-12-16 17:51:42 +0900

    チャット催促メッセージを開始ボタン押した側と押された側で変化＆手直し

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,22 +3,17 @@ class ChatsController < ApplicationController
   before_action :banned
 
   def index
-    @user = current_user
     latest_message?
-
   end
 
   def show
-    @invited_user = User.find_by(userid: params[:id])
-    if @invited_user.nil?
+    @user = User.find_by(userid: params[:id])
+    if @user.nil?
       redirect_to profiles_path, notice: 'そのユーザーidは存在しませんでした'
       return
     end
-    if @room.nil?
-      in_room?
-    end
-    @room_id = @room.id
-    @message = Message.where(room_id: @room_id)
+    in_room?
+    @message = Message.where(room_id: @room.id)
   end
 end
 
@@ -28,14 +23,18 @@ def latest_message?
   @room_partner = []
   @latest_message = []
   @chatroom = Room.order(updated_at: :desc).where(started_userid: current_user.id).or(Room.order(updated_at: :desc).where(invited_userid: current_user.id))
-    if @chatroom.nil?
-      redirect_to profiles_path, notice: '誰かとお話してみましょう！'
-      return
-    end
+  if @chatroom.nil?
+    redirect_to profiles_path, notice: '誰かとお話してみましょう！'
+    return
+  end
   @chatroom.each do |cr|
     @message = Message.order(updated_at: :desc).find_by(room_id: cr.id)
     if @message.nil?
-      @latest_message << ''
+      if cr.started_userid == current_user.id
+        @latest_message << ''
+      else
+        @latest_message << nil
+      end
     else
       @latest_message << @message.content.delete("\n").slice(0..50)
     end
@@ -50,11 +49,11 @@ end
 
 
 def in_room?
-  @room = Room.find_by(started_userid: current_user.id, invited_userid: @invited_user.id)
+  @room = Room.find_by(started_userid: current_user.id, invited_userid: @user.id)
   if @room.nil?
-    @room = Room.find_by(started_userid: @invited_user.id, invited_userid: current_user.id)
+    @room = Room.find_by(started_userid: @user.id, invited_userid: current_user.id)
     if @room.nil?
-      @room = Room.create(started_userid: current_user.id, invited_userid: @invited_user.id)
+      @room = Room.create(started_userid: current_user.id, invited_userid: @user.id)
       @room.save
     end
   end
