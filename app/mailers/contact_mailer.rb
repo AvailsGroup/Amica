df5a6a0f2774959154a25ffc5a573c01d01db7d5
kei-ju <cake1200294@gn.iwasaki.ac.jp>
2021-12-17 11:53:22 +0900

    通報昨日作成開始

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -8,6 +8,6 @@ class ContactMailer < ApplicationMailer
   def report_mail(report)
     @report = report
     @email = report.user.email
-    mail to: @email, bcc: "report+avails2022@gmail.com",subject:"【Amica】お問い合わせ受付"
+    mail to: @email, bcc: "report+avails2022@gmail.com",subject:"【Amica】通報受付"
   end
 end
