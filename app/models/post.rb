ccc44c050a4c1004d147c4d7bcd399ab30dbf746
kei-ju <cake1200294@gn.iwasaki.ac.jp>
2021-12-18 01:54:08 +0900

    通報機能完成（仮）

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
 
   belongs_to :user
-
+  has_many :report
   has_many :comments, dependent: :destroy
   has_many :likes
 
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,9 @@
 class Report < ApplicationRecord
   belongs_to :user
-  has_one :post
-  has_one :community
-  validates :user, uniqueness: { scope: :post }
+  belongs_to :reported_user ,class_name: "User", optional: true
+  belongs_to :post, optional: true
+  belongs_to :community, optional: true
+  belongs_to :comment, optional: true
+
+  validates :user, uniqueness: { scope: [:reported_user_id,:post_id,:community_id]}
 end
