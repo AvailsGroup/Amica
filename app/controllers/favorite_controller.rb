bda5d2b7580d545b576a3e668bb63623f967769d
yurisi0212 <Dev.yurisi@gmail.com>
2021-12-17 12:07:12 +0900

    コミュニティ画面からお気に入り登録できるように

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -13,13 +13,13 @@ class FavoriteController < ApplicationController
   end
 
   def community_create
-    Favorite.create!(user_id: current_user.id, community_id: params[:page_id])
+    Favorite.create(user_id: current_user.id, community_id: params[:page_id])
     flash[:notice] = "お気に入り登録しました!"
     redirect_back(fallback_location: pages_path)
   end
 
   def community_destroy
-    Favorite.find_by!(user_id: current_user.id, community_id: params[:page_id]).destroy
+    Favorite.find_by(user_id: current_user.id, community_id: params[:page_id]).destroy
     flash[:notice] = "お気に入り登録を解除しました!"
     redirect_back(fallback_location: pages_path)
   end
