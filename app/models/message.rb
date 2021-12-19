bccf11b02120aeaad2c03c1bed7fe76919e4aaae
skiwasaki <cask1200226@gn.iwasaki.ac.jp>
2021-12-19 21:23:28 +0900

    chatに画像投稿機能実装開始

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
   has_many :room
-  validates :content, presence: true
+  validates :user_id, presence: true
     # createの後にコミットする { MessageBroadcastJobのperformを遅延実行 引数はself }
   after_create_commit { MessageBroadcastJob.perform_later self }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_041919) do
+ActiveRecord::Schema.define(version: 2021_12_19_121743) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_12_13_041919) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "posts", force: :cascade do |t|
