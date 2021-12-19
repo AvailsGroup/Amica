54142b7f843535a31539c8d8dd1d727e80c47b9e
yurisi0212 <Dev.yurisi@gmail.com>
2021-12-19 16:35:36 +0900

    一瞬だけbase64がそのままデーターベースに入ってしまう不具合の修正

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -144,11 +144,11 @@ class CommunitiesController < ApplicationController
   def view_parameter
     @users = User.includes(:community_member, :tags)
     @user = @users.find(current_user.id)
-    @community = Community.includes(:community_members, :tags, :user,:favorites, :community_securities)
+    @community = Community.includes(:community_members, :tags, :user, :favorites, :community_securities)
   end
 
   def community_params
-    params.require(:community).permit(:name, :content, :image, :tag_list)
+    params.require(:community).permit(:name, :content, :tag_list)
   end
 
   def permission
