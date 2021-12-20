1cfed8129532162aedbd368452c2c68714dc1b17
yurisi0212 <dev.yurisi@gmail.com>
2021-12-20 11:09:33 +0900

    デバッグ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,13 +124,15 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  pp Rails.application.credentials.gmail[:email]
+  email = Rails.application.credentials.gmail[:email]
+  password = Rails.application.credentials.gmail[:password]
+  pp password
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
     port: 587,
-    user_name: Rails.application.credentials.gmail[:email],
-    password: Rails.application.credentials.gmail[:password],
+    user_name: email,
+    password: password,
     authentication: :login,
   }
 end
