32b800b8d09f208818b1e91c83066cb8ad0c64b0
yurisi0212 <dev.yurisi@gmail.com>
2021-12-20 11:25:19 +0900

    デバッグ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,18 +120,18 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'https://test-amica.herokuapp.com' } #URL
+  config.action_mailer.default_url_options = { host: 'https://test-amica.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  email = Rails.application.credentials.gmail[:email]
-  password = Rails.application.credentials.gmail[:password]
+
   config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
     address: 'smtp.gmail.com',
+    port: '587',
     domain: 'gmail.com',
-    port: 587,
-    user_name: email,
-    password: password,
-    authentication: :login,
+    authentication: 'plain',
+    user_name: Rails.application.credentials.gmail[:email],
+    password: Rails.application.credentials.gmail[:password]
   }
 end
