23ad8dd0e08f03add646ba37b924f926676672c9
n1200274 <cadu1200274@gn.iwasaki.ac.jp>
2021-12-20 11:20:48 +0900

    仮実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_04_143323) do
+ActiveRecord::Schema.define(version: 2021_12_12_191438) do
+
+  create_table "achievements", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.boolean "niceLv1", default: false
+    t.boolean "niceLv2", default: false
+    t.boolean "niceLv3", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "rare_like", default: false
+  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +50,14 @@ ActiveRecord::Schema.define(version: 2021_12_04_143323) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "communities", force: :cascade do |t|
     t.string "name"
     t.string "content"
@@ -58,11 +76,12 @@ ActiveRecord::Schema.define(version: 2021_12_04_143323) do
     t.index ["user_id"], name: "index_community_members_on_user_id"
   end
 
-  create_table "community_tags", force: :cascade do |t|
-    t.integer "community_id"
-    t.string "tag"
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "favorite_user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "community_id"
   end
 
   create_table "hashtags", force: :cascade do |t|
@@ -98,7 +117,8 @@ ActiveRecord::Schema.define(version: 2021_12_04_143323) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "userid"
+    t.integer "user_id"
+    t.string "image"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -109,6 +129,9 @@ ActiveRecord::Schema.define(version: 2021_12_04_143323) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "birthday"
+    t.text "twitter_id"
+    t.integer "enrolled_year"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
