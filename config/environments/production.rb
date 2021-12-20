f08371cd042da24f85cb764780412082cf7f1335
yurisi0212 <dev.yurisi@gmail.com>
2021-12-20 10:52:50 +0900

    メールが送れない不具合を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,7 +120,9 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'test-amica.herokuapp.com' } #URL
+  config.action_mailer.default_url_options = { host: 'https://test-amica.herokuapp.com' } #URL
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
