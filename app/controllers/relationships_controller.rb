9270ae5d24feeca3f97f63ec1343b6af1658b9dd
yurisi0212 <Dev.yurisi@gmail.com>
2021-12-21 22:46:43 +0900

    ゴミ排除

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,19 +5,22 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find_by(userid: params[:profile_id])
     Relationship.create(follower_id: current_user.id, followed_id: @user.id)
-    @notification = Notification.new(visitor_id: current_user.id, visited_id: @user.id,action: "follow",checked: false)
-    @notification.save
-    redirect_to profile_path(params[:profile_id]), notice: "フォローしました!"
+    @notification = Notification.create(visitor_id: current_user.id, visited_id: @user.id, action: 'follow')
+    redirect_to profile_path(params[:profile_id]), notice: 'フォローしました!'
   end
 
   def destroy
-    other_user = User.find_by(userid: params[:profile_id])
-    if Favorite.exists?(user_id: current_user.id, favorite_user_id: other_user.id)
-      Favorite.find_by(user_id: current_user.id, favorite_user_id: other_user.id).destroy
+    @user = User.find_by(userid: params[:profile_id])
+    if Favorite.exists?(user_id: current_user.id, favorite_user_id: @user.id)
+      Favorite.find_by(user_id: current_user.id, favorite_user_id: @user.id).destroy
     end
 
-    current_user.unfollow(other_user)
-    redirect_to profile_path(params[:profile_id]), notice: "フォローを解除しました！"
+    if Notification.exists?(visitor_id: current_user.id, visited_id: @user.id, action: 'follow', checked: false)
+      Notification.find_by(visitor_id: current_user.id, visited_id: @user.id, action: 'follow', checked: false).destroy
+    end
+
+    current_user.unfollow(@user)
+    redirect_to profile_path(params[:profile_id]), notice: 'フォローを解除しました！'
   end
 
 end
