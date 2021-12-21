262aa2181bc3ee65c10dbfe499ba3a64eda827d6
n1200274 <cadu1200274@gn.iwasaki.ac.jp>
2021-12-21 11:34:10 +0900

    チュートリアルの仮完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,10 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    if resource.sign_in_count == 1
-      users_tutorial_path
+    pp resource.sign_in_count
+    pp resource
+    if resource.sign_in_count  == 1
+      return users_tutorial_path
     end
       pages_path
   end
