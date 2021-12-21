30a6d6328432a884581a9bc4a538429c64f27ea7
yurisi0212 <Dev.yurisi@gmail.com>
2021-12-21 23:30:55 +0900

    通報を完成

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -9,7 +9,6 @@ class TimelinesController < ApplicationController
 
   def index
     view_parameter
-    @report = Report.new
     @posts = @posts.select { |p| matchers?(@user, p.user) || p.user == current_user }
     @posts = Kaminari.paginate_array(@posts).page(params[:page]).per(30)
     redirect 'friends'
@@ -88,6 +87,7 @@ class TimelinesController < ApplicationController
     @users = User.includes(:likes, :comments, :tags, :followings, :followers, :passive_relationships, :active_relationships)
     @user = @users.find(current_user.id)
     @create = Post.new
+    @report = Report.new
   end
 
   def post_params
