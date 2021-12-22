b92ddc7d779793fe91620761a2c96807f9034a6d
yurisi0212 <Dev.yurisi@gmail.com>
2021-12-22 21:30:46 +0900

    profiles showのブロック完成

--- app/controllers/block_controller.rb
+++ app/controllers/block_controller.rb
@@ -1,19 +1,21 @@
 class BlockController < ApplicationController
   def create
-    @users = User.includes[:block]
-    @user = @users.find_by(userid: params[:profile_id])
+    @users = User.includes(:blocks)
+    @user = @users.find(params[:profile_id])
     current_user?
     Block.create(user_id: current_user.id, blocked_user_id: @user.id)
     # TODO: destroy follow and favorite
-    # TODO: redirect
+    flash[:notice] = "ユーザーをブロックしました"
+    redirect_back fallback_location: pages_path
   end
 
   def destroy
-    @users = User.includes[:block]
-    @user = @users.find_by(userid: params[:profile_id])
+    @users = User.includes(:blocks)
+    @user = @users.find(params[:profile_id])
     current_user?
     Block.find_by(user_id: current_user.id, blocked_user_id: @user.id).destroy
-    # TODO: redirect
+    flash[:notice] = "ユーザーのブロックを解除しました"
+    redirect_back fallback_location: pages_path
   end
 
   private
