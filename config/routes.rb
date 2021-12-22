1ed1127e9dee348e8f6ba37f57ed595cef326fa1
yurisi0212 <Dev.yurisi@gmail.com>
2021-12-22 09:30:12 +0900

    masterをマージ

--- config/routes.rb
+++ config/routes.rb
@@ -92,6 +92,6 @@ Rails.application.routes.draw do
   resources :notifications, only: %i[index destroy]
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  # mount LetterOpenerWeb::Engine, at: '/letter_opener'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
