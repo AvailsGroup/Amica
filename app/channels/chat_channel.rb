cc4fb36095cc9427ce51c03e6705c41c88b20644
skiwasaki <cask1200226@gn.iwasaki.ac.jp>
2021-12-23 23:16:55 +0900

    チャット完成+大戦犯回避

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -11,43 +11,44 @@ class ChatChannel < ApplicationCable::Channel
   def speak(data)
     @room_id = data['room_id']
     @user_id = current_user.id
+    @f_name = nil
+    @image_data = nil
     unless File.directory?("#{Rails.root}/public/chats")
       Dir.mkdir("#{Rails.root}/public/chats/")
     end
-    if data['message'][0..21] == 'data:image/jpeg;base64'
+    if data['message'][0..4] == 'data:'
       unless File.directory?("#{Rails.root}/public/chats/room#{@room_id}")
         Dir.mkdir("#{Rails.root}/public/chats/room#{@room_id}")
       end
-      unless File.directory?("#{Rails.root}/public/chats/room#{@room_id}/images")
-        Dir.mkdir("#{Rails.root}/public/chats/room#{@room_id}/images")
-      end
-      rand = rand(1_000_000..9_999_999)
-      @image_data = "#{@user_id}#{rand}.jpg"
-      File.open("public/chats/room#{@room_id}/images/#{@image_data}", 'wb') do |f|
-        f.write(Base64.decode64(data['message']['data:image/jpeg;base64,'.length..-1]))
-      end
-      @content = '画像を投稿しました。'
-    elsif data['message'][0..15] == 'data:application'
-      unless File.directory?("#{Rails.root}/public/chats/room#{@room_id}")
-        Dir.mkdir("#{Rails.root}/public/chats/room#{@room_id}")
-      end
-      unless File.directory?("#{Rails.root}/public/chats/room#{@room_id}/files")
-        Dir.mkdir("#{Rails.root}/public/chats/room#{@room_id}/files")
-      end
-      r_start = data['message'].index('data:application')
       r_end = data['message'].index(',')
-      @replace = data['message'][r_start..r_end]
       n_start = data['message'].index('@')
-      @f_replace = data['message'][n_start..data['message'].length]
-      @f_name = data['message'][n_start.to_i + 1..data['message'].length - 1]
-      @base64 = data['message'][r_end.to_i + 1..n_start.to_i - 1]
-      File.open("public/chats/room#{@room_id}/files/#{@f_name}", 'wb') do |f|
-        f.write(Base64.decode64(@base64))
+      if r_end.nil? || n_start.nil?
+        @content = data['message']
+      else
+        @base64 = data['message'][r_end.to_i + 1..n_start.to_i - 1]
+        if data['message'][0..10] == 'data:image/'
+          unless File.directory?("#{Rails.root}/public/chats/room#{@room_id}/images")
+            Dir.mkdir("#{Rails.root}/public/chats/room#{@room_id}/images")
+          end
+          rand = rand(1_000_000..9_999_999)
+          @image_data = "#{@user_id}#{rand}.jpg"
+          File.open("public/chats/room#{@room_id}/images/#{@image_data}", 'wb') do |f|
+            f.write(Base64.decode64(@base64))
+          end
+          @content = '画像を投稿しました。'
+        else
+          @f_name = data['message'][n_start.to_i + 1..data['message'].length - 1]
+          unless File.directory?("#{Rails.root}/public/chats/room#{@room_id}/files")
+            Dir.mkdir("#{Rails.root}/public/chats/room#{@room_id}/files")
+          end
+          File.open("public/chats/room#{@room_id}/files/#{@f_name}", 'wb') do |f|
+            f.write(Base64.decode64(@base64))
+          end
+          @content = "#{@f_name}を送信しました。"
+        end
       end
-      @content = "【#{@f_name}】を送信しました。"
     else
       @content = data['message']
-      @image_data = nil
     end
     @message = Message.create content: @content, user_id: @user_id, room_id: @room_id, image: @image_data, file_name: @f_name
     @message.save
--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -17,27 +17,28 @@ const ChatChannel =  consumer.subscriptions.create("ChatChannel", {
     if (data.room_id === Number(room_id)){
       if (data.user_id === Number(user_id)) {
         var content ="";
-          if( data.image !== null){
-            content = '<img style="max-width:100%" data-lity="data-lity" src="/chats/room'+room_id+'/images/'+data.image+'">';
-          }else if (data.file_name !== null) {
-            content = '<a href="/chats/room1/files/' + data.file_name +'" download="'+ data.file_name +'">'+data.file_name+'</a>'
-          }else{
-            content = AutoLink(data.content);
-          }
+        if( data.image !== null){
+          content = '<img style="max-width:100%" data-lity="data-lity" src="/chats/room'+room_id+'/images/'+data.image+'">';
+        }else if (data.file_name !== null) {
+          content = '<a href="/chats/room1/files/' + data.file_name +'" download="'+ data.file_name +'">'+data.file_name+'</a>'
+        }else{
+          content = AutoLink(data.content);
+        }
 
-        $('#append').append('<div class="row message-body text-wrap " style="white-space: pre;">'+
-                              ' <div class="col-sm-12 message-main-sender" style=" position:relative;">'+
-                                  '<div class="sender my-1 p-1 mt-2" style="max-width: 40%;">' +
-                                    '<div class="messages container p-1">' +
-                                      content+
-                                    '</div>' +
-                                  '</div>'+
-                                    '<div class="text-gray small sender_time">'+
-                                     '今日 '+data.created_at.slice(11,16)+
-                                    '</div>'+
-                                 '</div>'+
-                                '</div>'+
-                                '<div class="clear"></div>'
+        $('#append').append(
+            '<div class="row message-body text-wrap " style="white-space: pre;">'+
+            ' <div class="col-sm-12 message-main-sender" style=" position:relative;">'+
+            '<div class="sender my-1 p-1 mt-2" style="max-width: 40%;">' +
+            '<div class="messages container p-1">' +
+            content+
+            '</div>' +
+            '</div>'+
+            '<div class="text-gray small sender_time">'+
+            '今日 '+data.created_at.slice(11,16)+
+            '</div>'+
+            '</div>'+
+            '</div>'+
+            '<div class="clear"></div>'
         )
         bottom_scroll();
       } else {
@@ -54,25 +55,25 @@ const ChatChannel =  consumer.subscriptions.create("ChatChannel", {
         }else{
           content = AutoLink(data.content);
         }
-      $('#append').append(
-                            '<div class="row message-body text-wrap " style="white-space: pre;">'+
-                              '<div class="col-sm-12 message-main-receiver" style=" position:relative;">'+
-                                '<a class="userLink" href="/profiles/'+ partneruserid + '">'+
-                                '  <img class="icon bd-placeholder-img flex-shrink-0 me-2 mt-2" style="float: left" width="40px" height="40px" src='+ partnerimage+'>' +
-                                '</a>'+
-                                '<div class="receiver my-1 p-1 mt-2" style="max-width: 40%;">' +
-                                  '<div class="messages container p-1">' +
-                                     content+
-                                  '</div>' +
-                                '</div>'+
-                               '<div class="text-gray small" style="float: left">' +
-                                  '<p class="time">'+
-                                  '今日 '+data.created_at.slice(11,16)+
-                                  '</p>'+
-                                '</div>'+
-                              '</div>'+
-                             '</div>'+
-                            '<div class="clear"></div>'
+        $('#append').append(
+            '<div class="row message-body text-wrap " style="white-space: pre;">'+
+            '<div class="col-sm-12 message-main-receiver" style=" position:relative;">'+
+            '<a class="userLink" href="/profiles/'+ partneruserid + '">'+
+            '<img class="icon bd-placeholder-img flex-shrink-0 me-2 mt-2" style="float: left" width="40px" height="40px" src='+ partnerimage+'>' +
+            '</a>'+
+            '<div class="receiver my-1 p-1 mt-2" style="max-width: 40%;">' +
+            '<div class="messages container p-1">' +
+            content+
+            '</div>' +
+            '</div>'+
+            '<div class="text-gray small" style="float: left">' +
+            '<p class="time">'+
+            '今日 '+data.created_at.slice(11,16)+
+            '</p>'+
+            '</div>'+
+            '</div>'+
+            '</div>'+
+            '<div class="clear"></div>'
         )
         bottom_scroll();
       }
@@ -84,121 +85,98 @@ const ChatChannel =  consumer.subscriptions.create("ChatChannel", {
   }
 });
 
+
+
 window.addEventListener("DOMContentLoaded",function() {
   const content = document.getElementById('content');
   const room_id = document.getElementById('room_id');
-  const images = document.getElementById('image_uploader')
-  const send_image = document.getElementById('send_image')
-  const files = document.getElementById('file_uploader')
-
+  const file_uploader = document.getElementById('file_uploader')
+  const preview = document.getElementById("preview")
 
   //改行や文字数に応じてtextareaを伸び縮みさせる
-    let $textarea = $('#content');
-    const lineHeight = parseInt($textarea.css('lineHeight'));
-    // 最低行数を指定
-    let minHeight = lineHeight;
-    // 最高幅を指定
-    let maxHeight = parseInt($(window).height() * 0.5);
-    $textarea.on('input', function() {
-      let lines = ($(this).val() + '\n').match(/\n/g).length;
-      $(this).height(Math.min(maxHeight, Math.max(lineHeight * lines, minHeight)));
-      if ( content.value === "") {
-        $(this).height(0);
-      }
-    });
-  //Shift+Enter or 紙飛行機ボタンでメッセ➖ジを送信させる
-    $(document).on('keypress', '[data-behavior~=chat_speaker]', function(event) {
-      if(event.shiftKey) {
-        if (event.key === 'Enter' && event.target.value && event.target.value.match(/\S/g)) {
-          ChatChannel.speak(event.target.value, room_id.value);
-          bottom_scroll()
-          event.target.value = '';
-          $($textarea).height(0);
-          return event.preventDefault();
-        }
-      }
-    });
-    $('#submit_button').click('[data-behavior~=chat_speaker]', function () {
-      if ( content.value && content.value.match(/\S/g)) {
-        const message = content.value
-        ChatChannel.speak(message, room_id.value);
-        bottom_scroll()
-        content.value = '';
-        $($textarea).height(0);
-        return content.preventDefault();
-      }
-    });
-  $('#image_uploader').click(function(){
-    send_image.value =""
-    var canvas = $("#canvas");
-    let base64
-    $(this).val('');
-    $(images).change(function() {
-      let file = ""
-      file = this.files[0];
-      if (!file.type.match(/^image\/(png|jpeg|gif)$/)) return;
-      var image = new Image();
-      var reader = new FileReader();
-      reader.onload = function(evt) {
-        image.onload = function() {
-          $(canvas).attr("width",image.width);
-          $(canvas).attr("height",image.height);
-          var ctx = canvas[0].getContext("2d");
-          ctx.drawImage(image, 0, 0); //canvasに画像を転写
-         　base64 =  canvas[0].toDataURL('image/jpeg');
-         send_image.value = base64
-        }
-        image.src = evt.target.result;
-      }
-      reader.readAsDataURL(file);
-    });
-  });
-  $('#image_submit_button').click('[data-behavior~=chat_speaker]', function () {
-    if ( send_image.value != null && !content.value.match(/\S/g) ) {
-      ChatChannel.speak(send_image.value,room_id.value);
-      send_image.value = '';
-      $($textarea).height(0);
-      $('#imageModal').modal('hide');
-      $('#image_uploader').val('');
-      bottom_scroll()
-      location.reload()
+  let $textarea = $('#content');
+  const lineHeight = parseInt($textarea.css('lineHeight'));
+  // 最低行数を指定
+  let minHeight = lineHeight;
+  // 最高幅を指定
+  let maxHeight = parseInt($(window).height() * 0.5);
+  $textarea.on('input', function() {
+    let lines = ($(this).val() + '\n').match(/\n/g).length;
+    $(this).height(Math.min(maxHeight, Math.max(lineHeight * lines, minHeight)));
+    if ( content.value === "") {
+      $(this).height(0);
     }
   });
-  $(files).click(function(){
-    var val = ""
-    let file_name = ""
+  //Shift+Enter or 紙飛行機ボタンでメッセ➖ジを送信させる
+  $(document).on('keypress', '[data-behavior~=chat_speaker]', function(event) {
+    if(event.shiftKey) {
+      if (event.key === 'Enter' && content.value) {
+        $(file_uploader).val('');
+        ChatChannel.speak(content.value, room_id.value);
+        bottom_scroll()
+        event.target.value = '';
+        $($textarea).height(0);
+        return event.preventDefault();
+      }
+    }
+  });
+  $('#submit_button').click('[data-behavior~=chat_speaker]', function () {
+    if ( content.value && content.value.match(/\S/g)) {
+      const message = content.value
+      ChatChannel.speak(message, room_id.value);
+      bottom_scroll()
+      content.value = '';
+      $($textarea).height(0);
+      return content.preventDefault();
+    }
+  });
+
+
+  $('#file_button').click(function(){
+    preview.style.display="none";
+    preview.src = null
+    $(file_uploader).val('');
+  });
+  $(file_uploader).click(function (){
+    preview.style.display="none";
+    preview.src = null
     $(this).val('');
-      const maxFileSize=10485760 //アップロードできる最大サイズを指定(1048576=1MB 10485760=10MB)
-      $(files).change(function(){//アップロードファイル取得//ファイルがアップロードされたら
-        let uploaded_file = ""
-        $(".error_msg").remove()　//エラーメッセージ削除
-        uploaded_file = $(this).prop('files')[0] //アップロードファイル取得
-        console.log(uploaded_file)
-        if (uploaded_file.type.match(/^image\/(png|jpeg|gif)$/)) {
-          $(this).val("")　//画像を空にする
-          $(this).before("<p class='error_msg'>こちらから画像は送信できません。</p>") //エラーメッセージ表示
-        }else if(maxFileSize < uploaded_file.size){ //もし上限値を超えた場合
-          $(this).val("")　//画像を空にする
-          $(this).before("<p class='error_msg'>アップロードできる最大サイズは10MBです</p>") //エラーメッセージ表示
-          console.log(uploaded_file)
-        }else {
-          $('#file_submit_button').click('[data-behavior~=chat_speaker]', function () {
-            var reader = new FileReader;
-            reader.readAsDataURL(uploaded_file);
-            reader.onload = function() {
-              val = reader.result;
-              file_name = uploaded_file.name
-              val = val + "@" + file_name
-              ChatChannel.speak(val, room_id.value);
-              $($textarea).height(0);
-              $('#fileModal').modal('hide');
-              $('#file_uploader').val('');
-              bottom_scroll()
-              location.reload()
-            }
-          })
-        }
-    });
+  });
+  $(file_uploader).change(function() {
+    if($(file_uploader).prop('files')[0].type.match(/^image\/(png|jpeg|gif)$/)) {
+      preview.style.display="";
+      var fileReader = new FileReader();
+      fileReader.onload = (function() {
+        preview.src = fileReader.result;
+      });
+      fileReader.readAsDataURL(this.files[0]);
+    }
+  });
+  $('#file_submit_button').click('[data-behavior~=chat_speaker]', function () {
+    const maxFileSize=10485760 //アップロードできる最大サイズを指定(1048576=1MB 10485760=10MB)
+    let val = null
+    $(".error_msg").remove()　//エラーメッセージ削除
+    let file = $(file_uploader).prop('files')[0];
+    if (maxFileSize < file.size) {
+      $(file_uploader).val("")
+      $(file_uploader).before("<p class='error_msg'>アップロードできる最大サイズは10MBです</p>") //エラーメッセージ表示
+    }else{
+      $(file_uploader).click(function () {
+        $(this).val("")
+      })
+      let reader = new FileReader;
+      reader.readAsDataURL(file);
+      reader.onload = function() {
+        val = reader.result;
+        let file_name = file.name
+        val = val + "@" + file_name
+        ChatChannel.speak(val, room_id.value);
+        $($textarea).height(0);
+        $('#fileModal').modal('hide');
+        $('#file_uploader').val('');
+        files_bottom_scroll()
+      }
+    }
   });
 });
 
@@ -206,12 +184,18 @@ function AutoLink(str) {
   var regexp_url = /((h?)(ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+))/g; // ']))/;
   var regexp_makeLink = function(all, url, h, href) {
     return '<a href="h' + href + '" target="_blank">' + url + '</a>';
-
   }
   let content;
-      content = str.replace(regexp_url, regexp_makeLink)
-     return  content.replace(/\r?\n/g, '<br>');
+  content = str.replace(regexp_url, regexp_makeLink)
+  return  content.replace(/\r?\n/g, '<br>');
+}
 
+function files_bottom_scroll(){
+  window.addEventListener('load', function (){
+    var elm = document.documentElement;
+    var bottom = elm.scrollHeight - elm.clientHeight;
+    window.scroll(0, bottom);
+  })
 }
 
 function bottom_scroll(){
@@ -219,3 +203,4 @@ function bottom_scroll(){
   var bottom = elm.scrollHeight - elm.clientHeight;
   window.scroll(0, bottom);
 }
+
