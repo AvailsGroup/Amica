4424b3e0fda28d058fd83ffcbcd3fafd658b47fa
yurisi0212 <Dev.yurisi@gmail.com>
2021-12-23 09:15:42 +0900

    ブロックしたユーザーの投稿をタイムラインから非表示

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,5 +4,5 @@ class Profile < ApplicationRecord
   validates :school_class, format: { with: /\A[0-9]\z/ }
   validates :number, format: { with: /\A[0-9]{1,2}\z/ }
   validates :student_id, format: { with: /\A[0-9]{7}\z/ }
-  validates :intro, length: { minimum: 0, maximum: 300 }
+  validates :intro, length: { minimum: 0, maximum: 300 }, allow_nil: true
 end
