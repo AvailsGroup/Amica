22b8383a204aad5512cd103539b984415efc3f1f
yurisi0212 <Dev.yurisi@gmail.com>
2021-12-27 05:18:56 +0900

    DRYに従った

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -5,15 +5,18 @@ class SettingsController < ApplicationController
   end
 
   def enable_enrolled_year
-    setting = Setting.find_by(user_id: current_user.id)
-    setting.visible_enrolled_year = true
-    setting.save
-    redirect_to settings_path
+    change_enrolled_year
   end
 
   def disable_enrolled_year
+    change_enrolled_year
+  end
+
+  private
+
+  def change_enrolled_year
     setting = Setting.find_by(user_id: current_user.id)
-    setting.visible_enrolled_year = false
+    setting.visible_enrolled_year = setting.visible_enrolled_year ? false : true
     setting.save
     redirect_to settings_path
   end
