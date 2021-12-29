b0837a616620697cad2e6b0240d8e266e049dacc
yurisi0212 <Dev.yurisi@gmail.com>
2021-12-29 18:20:05 +0900

    運営からのお知らせを作成開始

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_27_083923) do
+ActiveRecord::Schema.define(version: 2021_12_27_120000) do
 
   create_table "achievements", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -63,18 +63,6 @@ ActiveRecord::Schema.define(version: 2021_12_27_083923) do
     t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "admin_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
   create_table "blocks", force: :cascade do |t|
