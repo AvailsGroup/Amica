771d1b0dffa5fe5e7cca5f04d2e0031a1dab8acf
n1200294 <cake1200294@gn.iwasaki.ac.jp>
2021-12-29 18:44:31 +0900

    入学年のvalidation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
+  validate :can_register_enrolled_year
   validates :school_class, format: { with: /\A[0-9]\z/ }
   validates :number, format: { with: /\A[0-9]{1,2}\z/ }
   validates :student_id, format: { with: /\A[0-9]{7}\z/ }
@@ -10,4 +11,14 @@ class Profile < ApplicationRecord
   validates :instagram_id, format: { with: /\A(?!\.)[\w.]+(?<!\.)\z/ }, length: { minimum: 0, maximum: 30 }, allow_nil: true, allow_blank: true
   validates :discord_name, length: { minimum: 0, maximum: 32 }, allow_nil: true, allow_blank: true
   validates :discord_tag, format: { with: /\A[0-9]{4}\z/ }, length: { minimum: 0, maximum: 4 }, allow_nil: true, allow_blank: true
+
+  private
+
+  def can_register_enrolled_year
+    return if enrolled_year == nil?
+    if enrolled_year < 2000 || enrolled_year > DateTime.now.strftime('%Y').to_i
+      errors.add(:enrolled_year,"は不正な値です")
+    end
+  end
+
 end
