1bb43e1def171390677721e79b2cb7c23f78b46a
yurisi0212 <dev.yurisi@gmail.com>
2022-01-01 06:17:05 +0900

    運営からのメッセージ完成(遅延でコミットできてなかった)

--- db/migrate/20211231014733_create_whispers.rb
+++ db/migrate/20211231014733_create_whispers.rb
@@ -4,7 +4,7 @@ class CreateWhispers < ActiveRecord::Migration[6.1]
       t.integer :user_id, null: false
       t.string :title, null: false
       t.text :content, null: false
-      t.boolean :checked, null: false, default: true
+      t.boolean :checked, null: false, default: false
 
       t.timestamps
     end
