f35187c2b1ea00bc8910ccb5c4bd8724d022df66
yurisi <dev.yurisi@gmail.com>
2022-01-02 10:33:29 +0900

    チャットが通知に飛ぶように

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -6,7 +6,7 @@ class Notification < ApplicationRecord
   belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id', optional: true
   belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
 
-  ACTION_VALUES = %w[like follow comment].freeze
+  ACTION_VALUES = %w[like follow comment chat].freeze
   validates :action,  presence: true, inclusion: { in: ACTION_VALUES }
   validates :checked, inclusion: { in: [true, false] }
 end
