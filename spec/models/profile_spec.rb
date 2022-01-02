d7037c68d113c66d89773ecb2769303caec089b1
yurisi0212 <dev.yurisi@gmail.com>
2022-01-02 20:19:14 +0900

    プロフィールのテストを少し進めた

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Profile, type: :model do
         grade: 'テスト学科',
         school_class: 1,
         number: 10,
-        enrolled_year: 2002,
+        enrolled_year: DateTime.now.strftime("%Y").to_i,
         student_id: '1234567',
         intro: 'よろしくおねがいします',
         twitter_id: 'test',
@@ -22,6 +22,39 @@ RSpec.describe Profile, type: :model do
     end
   end
 
+  describe '#enrolled_year' do
+    context '規定の年より少ない場合' do
+      it 'バリデーションエラーを出す' do
+        profile = Profile.new(
+          id: 1,
+          user: User.new(id: 1),
+          enrolled_year: 1999
+          )
+        expect(profile).not_to be_valid
+      end
+    end
+    context '規定の年より多い場合' do
+      it 'バリデーションエラーを出す' do
+        profile = Profile.new(
+          id: 1,
+          user: User.new(id: 1),
+          enrolled_year: DateTime.now.strftime("%Y").to_i + 1
+        )
+        expect(profile).not_to be_valid
+      end
+    end
+    context '規定の年の場合' do
+      it '正常に保存される' do
+        profile = Profile.new(
+          id: 1,
+          user: User.new(id: 1),
+          enrolled_year: DateTime.now.strftime("%Y").to_i
+        )
+        expect(profile).to be_valid
+      end
+    end
+  end
+
 
   describe '#twitter_id' do
     context '日本語が使われている場合' do
@@ -30,7 +63,7 @@ RSpec.describe Profile, type: :model do
           id: 1,
           user: User.new(id: 1),
           twitter_id: 'てすと',
-          enrolled_year: 2002,
+          enrolled_year: DateTime.now.strftime("%Y").to_i
         )
         expect(profile).not_to be_valid
       end
@@ -42,11 +75,50 @@ RSpec.describe Profile, type: :model do
           id: 1,
           user: User.new(id: 1),
           twitter_id: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-          enrolled_year: 2002,
+          enrolled_year: DateTime.now.strftime("%Y").to_i
         )
         expect(profile).not_to be_valid
       end
     end
+  end
+
+  describe '#instagram_id' do
+    context '30文字以上の場合' do
+      it 'バリデーションエラーを出す' do
+        profile = Profile.new(
+          id: 1,
+          user: User.new(id: 1),
+          instagram_id: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+          enrolled_year: DateTime.now.strftime("%Y").to_i
+        )
+        expect(profile).not_to be_valid
+      end
+    end
+
+    context '日本語が含まれている場合' do
+      it 'バリデーションエラーを出す' do
+        profile = Profile.new(
+          id: 1,
+          user: User.new(id: 1),
+          instagram_id: 'テスト',
+          enrolled_year: DateTime.now.strftime("%Y").to_i
+        )
+        expect(profile).not_to be_valid
+      end
+    end
+
+    context 'インスタグラムのidのフォーマットに適合する場合' do
+      it '正常に保存される' do
+        profile = Profile.new(
+          id: 1,
+          user: User.new(id: 1),
+          instagram_id: 'test123',
+          enrolled_year: DateTime.now.strftime("%Y").to_i
+        )
+        expect(profile).to be_valid
+      end
+    end
+
   end
 
 end
