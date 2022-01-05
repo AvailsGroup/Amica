1935b10a245729349d2f1e20ed0c7f2aaa99cb24
yurisi <dev.yurisi@gmail.com>
2022-01-05 19:57:48 +0900

    あんま見えちゃいけないものが見えてた不具合を修正

--- app/javascript/packs/chat/show.js
+++ app/javascript/packs/chat/show.js
@@ -9,10 +9,10 @@ window.addEventListener("DOMContentLoaded", function (utterance) {
     let num
     $('.morebtn').click(function () {
         startnum = startnum - 50
-        if(startnum<0){
+        if (startnum < 0) {
             num = startnum + 49
             startnum = 0
-        }else {
+        } else {
             num = startnum + 49
         }
         messageHash.slice(startnum, num + 1).forEach(function () {
@@ -22,12 +22,12 @@ window.addEventListener("DOMContentLoaded", function (utterance) {
             }
 
             if (messageHash[num].file_name !== null) {
-                content = '<a href="' + messageHash[num].url + '" download="' + messageHash[num].file_name.slice(1,messageHash[num].file_name.length) + '">'
-                    + messageHash[num].file_name.slice(1,messageHash[num].file_name.length) + '</a>'
+                content = '<a href="' + messageHash[num].url + '" download="' + messageHash[num].file_name.slice(1, messageHash[num].file_name.length) + '">'
+                    + messageHash[num].file_name.slice(1, messageHash[num].file_name.length) + '</a>'
             }
 
             if (partner_image === "") {
-                partner_image = "/assets/default_icon.png"
+                partner_image = "/public/default_icon.png"
             }
             let today = new Date();
             let time
@@ -35,19 +35,19 @@ window.addEventListener("DOMContentLoaded", function (utterance) {
             num > 0 ? send_time = new Date(messageHash[num].created_at) - new Date(messageHash[num - 1].created_at) : send_time = new Date(messageHash[num].created_at)
             let time_ber = ''
             let time_flag = document.getElementById(messageHash[num].created_at.slice(0, 10))
-            if(Math.floor(send_time/1000/60/60/24) >= 1 || num === startnum){
-                time_ber = '<div id="'+messageHash[num].created_at.slice(0, 10)+'" class="date">'+messageHash[num].created_at.slice(0, 10)+'</div>'
+            if (Math.floor(send_time / 1000 / 60 / 60 / 24) >= 1 || num === startnum) {
+                time_ber = '<div id="' + messageHash[num].created_at.slice(0, 10) + '" class="date">' + messageHash[num].created_at.slice(0, 10) + '</div>'
             }
-            if(time_flag != null)time_flag.style.display="none";
-            today = today.getFullYear() + "-" +  ("0" + (today.getMonth()+1)).slice(-2) + "-"+ ("0" + (today.getDate())).slice(-2)
+            if (time_flag != null) time_flag.style.display = "none";
+            today = today.getFullYear() + "-" + ("0" + (today.getMonth() + 1)).slice(-2) + "-" + ("0" + (today.getDate())).slice(-2)
             if (messageHash[num].created_at.slice(0, 10) === today) {
-                 time = '今日 ' + messageHash[num].created_at.slice(11, 16)}
-            else {
+                time = '今日 ' + messageHash[num].created_at.slice(11, 16)
+            } else {
                 time = messageHash[num].created_at.slice(11, 16)
             }
 
 
-            let html = time_ber+
+            let html = time_ber +
                 '<div class="row message-body text-wrap" style="white-space: pre;">' +
                 '<div class="col-sm-12 message-main-receiver" style=" position:relative;">' +
                 '<a class="userLink" href="/profiles/' + partner_userid + '">' +
@@ -66,8 +66,8 @@ window.addEventListener("DOMContentLoaded", function (utterance) {
                 '</div>' +
                 '</div>' +
                 '<div class="clear"></div>'
-            if (Number(messageHash[num].user_id) === Number(user_id.value)){
-                html = time_ber+
+            if (Number(messageHash[num].user_id) === Number(user_id.value)) {
+                html = time_ber +
                     '<div class="row message-body text-wrap" style="white-space: pre;">' +
                     ' <div class="col-sm-12 message-main-sender" style=" position:relative;">' +
                     '<div class="sender my-1 p-1 mt-2" style="max-width: 40%;">' +
@@ -83,11 +83,11 @@ window.addEventListener("DOMContentLoaded", function (utterance) {
                     '<div class="clear"></div>'
             }
 
-                $(html).prependTo('#messages');
-
+            $(html).prependTo('#messages');
             num = num - 1
         })
     })
+
     function AutoLink(str) {
         var regexp_url = /((h?)(ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+))/g;
         var regexp_makeLink = function (all, url, h, href) {
