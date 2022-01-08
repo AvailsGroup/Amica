6e239411b631c387fb6442ee489c950b990ed34b
skiwasaki <cask1200226@gn.iwasaki.ac.jp>
2022-01-08 11:24:22 +0900

    chatのバグ修正

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -13,58 +13,58 @@ class ChatChannel < ApplicationCable::Channel
   def speak(data)
     room = Room.find_by(id: data['room_id'])
     if room.started_user_id == current_user.id || room.invited_user_id == current_user.id
-    @room_id = data['room_id']
-    @image_name, @url, @f_name = nil
-    @type = data['content_type']
-    @content = data['message']
-    Dir.mkdir("#{Rails.root}/tmp/chats/") unless File.directory?("#{Rails.root}/tmp/chats")
-    unless File.directory?("#{Rails.root}/tmp/chats/room#{@room_id}")
-      Dir.mkdir("#{Rails.root}/tmp/chats/room#{@room_id}")
-    end
-    if @type == 'file'
-      r_end = data['message'].index(',')
-      n_start = data['message'].index('@')
-      @base64 = @content[r_end.to_i + 1..n_start.to_i - 1]
-      if @content[0..10] == 'data:image/'
-        rand = rand(1_000_000..9_999_999)
-        @image_name = "#{rand}.jpg"
-        File.open("#{Rails.root}/tmp/chats/room#{@room_id}/#{@image_name}", 'wb+') do |f|
-          f.write(Base64.decode64(@base64))
-        end
-        f = File.open("#{Rails.root}/tmp/chats/room#{@room_id}/#{@image_name}")
-        Message.first.images.attach(io: f, filename: @image_name)
-        f.close
-        File.delete("#{Rails.root}/tmp/chats/room#{@room_id}/#{@image_name}")
-        @type = 'image'
-        @url = url_for(ActiveStorage::Blob.find_by(filename: @image_name))
-      else
-        @f_name = (data['message'][n_start.to_i + 1..data['message'].length - 1]).to_s
-        File.open("#{Rails.root}/tmp/chats/room#{@room_id}/#{@f_name}", 'wb+') do |f|
-          f.write(Base64.decode64(@base64))
-        end
-        f = File.open("#{Rails.root}/tmp/chats/room#{@room_id}/#{@f_name}")
-        Message.first.files.attach(io: f, filename: @f_name)
-        f.close
-        File.delete("#{Rails.root}/tmp/chats/room#{@room_id}/#{@f_name}")
-        @url = rails_blob_url(ActiveStorage::Blob.find_by(filename: @f_name), disposition: 'attachment')
+      @room_id = data['room_id']
+      @image_name, @url, @f_name = nil
+      @type = data['content_type']
+      @content = data['message']
+      Dir.mkdir("#{Rails.root}/tmp/chats/") unless File.directory?("#{Rails.root}/tmp/chats")
+      unless File.directory?("#{Rails.root}/tmp/chats/room#{@room_id}")
+        Dir.mkdir("#{Rails.root}/tmp/chats/room#{@room_id}")
       end
-      @content = 'content'
+      if @type == 'file'
+        r_end = data['message'].index(',')
+        n_start = data['message'].index('@')
+        @base64 = @content[r_end.to_i + 1..n_start.to_i - 1]
+        if @content[0..10] == 'data:image/'
+          rand = rand(1_000_000..9_999_999)
+          @image_name = "#{rand}.jpg"
+          File.open("#{Rails.root}/tmp/chats/room#{@room_id}/#{@image_name}", 'wb+') do |f|
+            f.write(Base64.decode64(@base64))
+          end
+          f = File.open("#{Rails.root}/tmp/chats/room#{@room_id}/#{@image_name}")
+          Message.first.images.attach(io: f, filename: @image_name)
+          f.close
+          File.delete("#{Rails.root}/tmp/chats/room#{@room_id}/#{@image_name}")
+          @type = 'image'
+          @url = url_for(ActiveStorage::Blob.find_by(filename: @image_name))
+        else
+          @f_name = (data['message'][n_start.to_i + 1..data['message'].length - 1]).to_s
+          File.open("#{Rails.root}/tmp/chats/room#{@room_id}/#{@f_name}", 'wb+') do |f|
+            f.write(Base64.decode64(@base64))
+          end
+          f = File.open("#{Rails.root}/tmp/chats/room#{@room_id}/#{@f_name}")
+          Message.first.files.attach(io: f, filename: @f_name)
+          f.close
+          File.delete("#{Rails.root}/tmp/chats/room#{@room_id}/#{@f_name}")
+          @url = rails_blob_url(ActiveStorage::Blob.find_by(filename: @f_name), disposition: 'attachment')
+        end
+        @content = 'content'
+      end
+      @message = Message.create content: @content, user_id: current_user.id, room_id: @room_id, image: @image_name,
+                                file_name: @f_name, url: @url, content_type: @type
+  
+      room = Room.find_by(id: @room_id)
+  
+      other_user_id = room.started_user_id == current_user.id ? room.invited_user_id : room.started_user_id
+  
+      unless Notification.exists?(visitor_id: current_user.id, visited_id: other_user_id, action: 'chat', checked: false)
+        Notification.create(visitor_id: current_user.id, visited_id: other_user_id, action: 'chat')
+      end
+  
+      @room = room.touch(:created_at)
+      ActionCable.server.broadcast 'chat_channel', @message
     end
-    @message = Message.create content: @content, user_id: current_user.id, room_id: @room_id, image: @image_name,
-                              file_name: @f_name, url: @url, content_type: @type
-
-    room = Room.find_by(id: @room_id)
-
-    other_user_id = room.started_user_id == current_user.id ? room.invited_user_id : room.started_user_id
-
-    unless Notification.exists?(visitor_id: current_user.id, visited_id: other_user_id, action: 'chat', checked: false)
-      Notification.create(visitor_id: current_user.id, visited_id: other_user_id, action: 'chat')
-    end
-
-    @room = room.touch(:created_at)
-    ActionCable.server.broadcast 'chat_channel', @message
-    end
-    end
+  end
 end
 
 
