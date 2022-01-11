6f8df75b86e409e1f339cd55f6fd72ab9b36d5cd
yurisi0212 <dev.yurisi@gmail.com>
2022-01-11 23:36:33 +0900

    コミュニティに参加していない場合はチャットルームに入れないように

--- app/controllers/communities_room_controller.rb
+++ app/controllers/communities_room_controller.rb
@@ -4,6 +4,9 @@ class CommunitiesRoomController < ApplicationController
 
   def show
     @community = Community.find(params[:community_id])
+    unless current_user.community_member.any? { |c| c.community_id == @community.id }
+      redirect_back fallback_location: communities_path, notice: "コミュニティに参加していません"
+    end
     @messages = @community.community_messages
     @messages.to_json(only: [:content, :image, :file_name, :created_at, :url])
   end
