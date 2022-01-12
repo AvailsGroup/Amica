b8c9a9b1a71c9abd28de0c17f1969e587b7be1d7
yurisi0212 <dev.yurisi@gmail.com>
2022-01-13 01:15:28 +0900

    本番環境実装

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,7 +124,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'https://avais-amica.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://www.avails-amica.com/' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
