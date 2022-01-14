6fa688a5e2fd29a7a13dee9c8dda8c80d719592a
yurisi0212 <dev.yurisi@gmail.com>
2022-01-15 05:30:09 +0900

    デザインを変更

--- app/javascript/packs/home/index.js
+++ app/javascript/packs/home/index.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
     $(".goSection").on("click", function() {
         const scrollTarget = $(this)[0].attributes[1].nodeValue;
         const offsetTop = $(scrollTarget).offset().top;
-        $("html, body").animate({ scrollTop: offsetTop }, 200);
+        $("html, body").animate({ scrollTop: offsetTop }, 600);
         return false;
     });
 });
