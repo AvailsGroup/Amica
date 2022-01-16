88033ee3591346abb12090e4c957c6f73e9e7c62
yurisi0212 <dev.yurisi@gmail.com>
2022-01-16 23:27:11 +0900

    設定に項目追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_152533) do
+ActiveRecord::Schema.define(version: 2022_01_16_141837) do
 
   create_table "achievements", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -246,6 +246,7 @@ ActiveRecord::Schema.define(version: 2022_01_13_152533) do
     t.boolean "visible_enrolled_year", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "visible_student_id", default: true, null: false
   end
 
   create_table "taggings", force: :cascade do |t|
