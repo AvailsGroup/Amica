9b70e41d08b63025388a07c1a11e73ba23c6149c
yurisi0212 <dev.yurisi@gmail.com>
2022-01-20 12:41:40 +0900

    バッジを修正、リファクタリング

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -2,4 +2,16 @@ module ProfileHelper
   def block_color(user)
     blocked?(current_user, user) ? 'blue' : 'red'
   end
+
+  def badge_color(rare)
+    return 'gold' if rare == 'very_high'
+
+    return 'red' if rare == 'high'
+
+    return 'greenyellow' if rare == 'middle'
+
+    return 'deepskyblue' if rare == 'low'
+
+    'rgba(29,144,190,0.8)'
+  end
 end
