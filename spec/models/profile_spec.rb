90e54e96bcfac34a698d424cb297a6aa7c419d47
yurisi0212 <dev.yurisi@gmail.com>
2022-01-20 15:11:28 +0900

    profileのテストを途中まで記述

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Profile, type: :model do
   end
 
   describe '#school_class' do
-    context '3文字以上の場合' do
+    context '3文字を超える場合' do
       it 'エラーを出す' do
         profile = build(:profile)
         profile.school_class = 123
@@ -26,8 +26,52 @@ RSpec.describe Profile, type: :model do
     end
   end
 
+  describe '#number' do
+    context '3文字を超える場合' do
+      it 'エラーを出す' do
+        profile = build(:profile)
+        profile.number = 123
+        expect(profile).not_to be_valid
+      end
+    end
+
+    context 'ブランクの場合' do
+      it 'エラーを出す' do
+        profile = build(:profile)
+        profile.number = ''
+        expect(profile).not_to be_valid
+      end
+    end
+  end
+
+  describe '#student_id' do
+    context '7文字を超える場合' do
+      it 'エラーを出す' do
+        profile = build(:profile)
+        profile.student_id = '12345678'
+        expect(profile).not_to be_valid
+      end
+    end
+
+    context '7文字未満の場合' do
+      it 'エラーを出す' do
+        profile = build(:profile)
+        profile.student_id = '123456'
+        expect(profile).not_to be_valid
+      end
+    end
+
+    context 'ブランクの場合' do
+      it 'エラーを出す' do
+        profile = build(:profile)
+        profile.student_id = ''
+        expect(profile).not_to be_valid
+      end
+    end
+  end
+
   describe '#enrolled_year' do
-    context '規定の年より少ない場合' do
+    context '規定の年未満の場合' do
       it 'エラーを出す' do
         profile = build(:profile)
         profile.enrolled_year = 1999
@@ -35,7 +79,7 @@ RSpec.describe Profile, type: :model do
       end
     end
 
-    context '規定の年より多い場合' do
+    context '規定の年を超える場合' do
       it 'エラーを出す' do
         profile = build(:profile)
         profile.enrolled_year = DateTime.now.strftime('%Y').to_i + 1
@@ -53,7 +97,7 @@ RSpec.describe Profile, type: :model do
       end
     end
 
-    context '15文字以上の場合' do
+    context '15文字を超える場合' do
       it 'エラーを出す' do
         profile = build(:profile)
         profile.twitter_id = '1234567890123456'
@@ -71,7 +115,7 @@ RSpec.describe Profile, type: :model do
   end
 
   describe '#instagram_id' do
-    context '30文字以上の場合' do
+    context '30文字を超える場合' do
       it 'エラーを出す' do
         profile = build(:profile)
         profile.instagram_id = '1234567890123456789012345678901'
@@ -97,7 +141,7 @@ RSpec.describe Profile, type: :model do
   end
 
   describe '#discord_name' do
-    context '名前が32文字以上の場合' do
+    context '名前が32文字を超える場合' do
       it 'バリデーションエラーが出る' do
         profile = build(:profile)
         profile.discord_name = '123456789012345678901234567890123'
@@ -115,18 +159,10 @@ RSpec.describe Profile, type: :model do
   end
 
   describe '#discord_tag' do
-    context '数字ではない場合' do
-      it 'エラーを出す' do
-        profile = build(:profile)
-        profile.discord_tag = 'test'
-        expect(profile).not_to be_valid
-      end
-    end
-
     context '0から始まる場合' do
       it 'エラーを出す' do
         profile = build(:profile)
-        profile.discord_tag = 0012
+        profile.discord_tag = '0012'
         expect(profile).not_to be_valid
       end
     end
@@ -136,6 +172,22 @@ RSpec.describe Profile, type: :model do
         profile = build(:profile)
         profile.discord_tag = ''
         expect(profile).to be_valid
+      end
+    end
+
+    context '4文字以内の場合' do
+      it '正常に保存される' do
+        profile = build(:profile)
+        profile.discord_tag = '12'
+        expect(profile).to be_valid
+      end
+    end
+
+    context '4文字を超える場合' do
+      it 'エラーが出る' do
+        profile = build(:profile)
+        profile.discord_tag = '12345'
+        expect(profile).not_to be_valid
       end
     end
   end
