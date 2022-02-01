e8c52dfe8fa793e33d9bbfd6e2883f5e9d3357ba
yurisi0212 <dev.yurisi@gmail.com>
2022-02-02 06:19:20 +0900

    99件以降は99+表示になるように

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -16,10 +16,13 @@ class NotificationsController < ApplicationController
     @comments = Comment.includes(:user)
     @content = notifications.where.not(visitor_id: user.id)
     @notification_count = notifications.where(checked: false).size
+    @notification_count = '99+' if @notification_count > 99
     informations = InformationShow.includes(:information, :user)
     information = informations.select { |i| i.user_id == user.id }
     @info_show_count = Information.all.size - information.size
+    @info_show_count = '99+' if @info_show_count > 99
     @whispers = user.whispers.where(checked: false).size
+    @whispers = '99+' if @whispers > 99
     @page = 'notification'
     @path = 'notifications/notification'
     render 'notifications/index'
